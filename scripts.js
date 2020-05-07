fae3f09a756c79695bcc105f9a5d4bc08a7dd768
jose javalera <javalera5975@yahoo.com>
2020-05-06 18:29:11 -0600

    added Time Stamp Greeting

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.getElementById('my-paragraph').innerHTML = 'Thank you for visiting my resume site!';
+
 
 let table = document.getElementById('references-table'); table.style.color = "Red"
 
@@ -6,12 +6,20 @@ $(document).ready(() => {
     $('#getUserName').modal('show');
 });
 
-function greetUser() {
-let name = document.getElementById('greetingName').value;
-document.getElementById('userName').innerHTML = name;
+
+
+
+
+function greetUser() 
+{
+    let name = document.getElementById('greetingName').value;
+    document.getElementById('userName').innerHTML = name;
+    $('#getUserName').modal('hide');
+    $('#greetUser').modal('show');
 }
 
-function getContactData() {
+function getContactData() 
+{
     let name = document.getElementById('name').value
     let phonenumber = document.getElementById('phonenumber').value;
     let emailaddress = document.getElementById('emailaddress').value;
@@ -19,6 +27,26 @@ function getContactData() {
     console.log(name);
     console.log(emailaddress);
     console.log(phonenumber);
-
-
 }
+/*---------------------time of day code--------------*/
+function custoizeGreeting() {
+    let hour = getTimeOfDay
+    let message = 'Early Bird Special';
+
+    if (hour >=5 && hour < 12) {
+        message = 'Good Morning';
+    } else if (hour >=12 && hour < 18) { 
+        message = 'Good afternoon';
+    } else if (hour >= 18) {
+        message = 'Good Evening and Good Night';
+    }
+}    
+
+
+function getTimeOfDay() 
+{
+ let now = new Date();
+ let hour = now.getHours();
+ return hour;
+}
+
