ed7fa52d94db1041ebefde15d340c0439cf128f2
jose javalera <javalera5975@yahoo.com>
2020-05-12 19:03:27 -0600

    Array2

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
-
-
-let table = document.getElementById('references-table'); table.style.color = "Red"
+let table = document.getElementById('references-table'); 
+table.style.color = "Red"
 
 $(document).ready(() => {
     $('#getUserName').modal('show');
-    custoizeGreeting();
+    customizeGreeting();
 });
-
-
-
-
 
 function greetUser() 
 {
@@ -29,30 +24,59 @@ function getContactData()
     console.log(emailaddress);
     console.log(phonenumber);
 }
+
 /*---------------------time of day code--------------*/
-function custoizeGreeting() 
+function customizeGreeting() 
 {
     let hour = getTimeOfDay();
 
-    if (hour >=5 && hour < 12) {
+    if (hour >=5 && hour < 12) 
+    {
         message = 'Good Morning';
-    } else if (hour >=12 && hour < 18) { 
+    } 
+    else if (hour >=12 && hour < 18)
+    { 
         message = 'Good afternoon';
-    } else if (hour >= 18) {
+    } 
+    else if (hour >= 18)
+    {
         message = 'Good Evening and Good Night';
     }
     displayGreetingMessage(message);
-
 }    
-
 
 function getTimeOfDay() 
 {
- let now = new Date();
- let hour = now.getHours();
- return hour;
+    let now = new Date();
+    let hour = now.getHours();
+    return hour;
 }
 
-function displayGreetingMessage (message) {
+function displayGreetingMessage (message) 
+{
     document.getElementById('message').innerHTML = message;
+}
+
+/* let exampleArray = new Array[3];
+let exampleArray2 = [1,2,3];
+let exampleArray3 = ["one etc.", "two seven", "three"]; */
+
+function displsyRreferences() {
+ let references = [
+    {company: "Company", contact: "Contact", contactInfo: "Contact Info"},
+    {company: "University of Utah Redwood Pharmacy", contact: "Machaela Jaquez", contactInfo: "801-213-9950"},
+    {company: "University of Utah Home Infusion", contact: "Jamie Doi", contactInfo: "801-587-8600"},
+ ];
+
+let tableBody = document.querySelector('#references-table>tbody');
+
+for (let reference of references) {
+    let html = '';
+    html += '<tr>';
+    html += '<td>' + reference.company + '</td>';
+    html += '<td>' + reference.contact + '</td>';
+    html += '<td>' + reference.contactInfo+ '</td>';
+    html += '</tr>';
+
+    tableBody.innerHTML += html;}
 }
